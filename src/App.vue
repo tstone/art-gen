622f6c906ff46ff24ed5adaa4b033ae796411513
Titus Stone <titus.stone@ibotta.com>
2023-11-10 17:49:10 -0700

    Convert to Vite/Vue project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <a href="watercolor.html">Abstract Watercolor-esque Experiments</a>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
